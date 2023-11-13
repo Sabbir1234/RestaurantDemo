8c164b471335bed32eb390a66b57320313dd7c65
Sabbir Hossain <sabbir.hossain@upaybd.com>
2023-11-13 13:14:08 +0600

    Initial Commit

---
+++ iDine/iDineApp.swift
@@ -0,0 +1,17 @@
+//
+//  iDineApp.swift
+//  iDine
+//
+//  Created by SABBIR HOSSAIN on 13/11/23.
+//
+
+import SwiftUI
+
+@main
+struct iDineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
